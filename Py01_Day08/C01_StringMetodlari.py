ad74ef4b67c5875bc9fc6e97760d09f07fb6f264
Your Name <ahmetkerembey72@gmail.com>
2026-01-20 17:43:41 +0100

    Py01_Day08

--- Py01_Day08/C01_StringMetodlari.py
+++ Py01_Day08/C01_StringMetodlari.py
@@ -1,6 +1,12 @@
-##     git init
-##     git add README.md
-##     git commit -m "first commit"
-##     git branch -M main
-##     git remote add origin https://github.com/KeremNomad/KeremsPython.git
-##     git push -u origin main
+me = "Benim adim Osman 19 yasindayim ve Urfaliyim"
+#1- tamami buyuk harfle yazin
+print (me.upper())
+#2- tamamini kucuk harfle yazin
+print (me.lower())
+#3- her kelimenin ilk harfi buyuk yapin
+print(me.title())
+#4- sadece ilk kelimenin harini buyuk yapin
+print(me.capitalize())
+#5- cumleyi bosluklarindan ayirarak yani bir liste elde ediniz
+print(me.split())
+
